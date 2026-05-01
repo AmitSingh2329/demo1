b58f8b89a58d246e5e1c69a5bc333ff6d0821f9e
AmitSingh2329 <amitsingh731803@gmail.com>
2026-05-01 09:41:29 +0530

    debug

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,8 +25,8 @@ export const registerUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
